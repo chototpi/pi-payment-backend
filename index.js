5f0f203373efd7c14b6aa08e25365f4a440e6813
chototpi <moonbix.bnb@gmail.com>
2025-06-14 12:39:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
   // API Key of your app, available in the Pi Developer Portal
   // DO NOT hardcode this, read it from an environment variable and treat it like a production secret.
-  const PI_API_KEY = "YOUR_PI_API_KEY"
+  const PI_API_KEY = "PI_API_KEY"
 
   const axiosClient = axios.create({baseURL: 'https://api.minepi.com', timeout: 20000});
   const config = {headers: {'Authorization': `Key ${PI_API_KEY}`, 'Content-Type': 'application/json'}};
@@ -35,7 +35,7 @@ piTestnet.fetchBaseFee().then(response => baseFee = response);
 
 // See the "Obtain your wallet's private key" section above to get this.
 // And DON'T HARDCODE IT, treat it like a production secret.
-const mySecretSeed = "S_YOUR_SECRET_SEED"; // NEVER expose your secret seed to public, starts with S
+const mySecretSeed = "APP_PRIVATE_KEY"; // NEVER expose your secret seed to public, starts with S
 const myKeypair = StellarSdk.Keypair.fromSecret(mySecretSeed);
 transaction.sign(myKeypair);
 
